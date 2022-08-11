020dcee0d285ba4f7db32fbd90396c112dc6e3b4
中田卓郎 <takurou403@gmail.com>
2022-08-11 20:20:08 +0900

    当案件で利用するテーブル定義等を作成

---
+++ app/Models/ChecklistTodoWork.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ChecklistTodoWork extends Model
+{
+    use HasFactory;
+}
---
+++ tests/Unit/Models/DutyStationTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Tests\Unit\Models;
+
+use PHPUnit\Framework\TestCase;
+
+class DutyStationTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function test_example()
+    {
+        $this->assertTrue(true);
+    }
+}
