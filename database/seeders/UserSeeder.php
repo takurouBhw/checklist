005eed40d192c25670335dbcd614022517b0f8c4
中田卓郎 <takurou403@gmail.com>
2022-08-13 11:32:51 +0900

    ユーザ情報修正と作成
    シーダー作成
    テーブル定義変更
    ダミーデータ作成
    ストアリクエスト作成

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => 'テスト太郎',
+            'user_id' => uniqid(),
+            'company_id' => 1,
+            'branch_office_id' => random_int(1, 10),
+            'duty_station_id' => random_int(1, 10),
+            'phone' => '0557553478',
+            'deleted_at' => null,
+            'role' => 2,
+            'email' => 'test@test.com',
+            'email_verified_at' => now(),
+            'password' => 'password',
+            'last_logined_at' => null,
+            'last_checklist_id' => null,
+        ]);
+    }
+}
