a027a8854fe262d0312d59be1d1704421f41cb9f
中田卓郎 <takurou403@gmail.com>
2022-08-15 09:50:55 +0900

    チェックリスト全ユーザー全件取得API実装

--- app/Http/Controllers/ChecklistController.php
+++ app/Http/Controllers/ChecklistController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Checklist;
 use Illuminate\Http\Request;
 
 class ChecklistController extends Controller
@@ -13,7 +14,8 @@ class ChecklistController extends Controller
      */
     public function index()
     {
-        //
+        $checklist = Checklist::findAllForAllUsers()->get();
+        return $checklist->toArray();
     }
 
     /**
